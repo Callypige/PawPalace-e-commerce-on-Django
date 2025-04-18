e1a81f216caa7989533f7239c1a8688cae28bee4
Callypige <sophie.nourry58@outlook.fr>
2025-04-18 17:28:28 +0200

    Create profile page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .models import CustomUser
+
+class AccoutsUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('email', 'username', 'is_staff', 'is_active')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
 
 admin.site.register(CustomUser, UserAdmin)
